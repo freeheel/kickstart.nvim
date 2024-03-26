0452617c68f78f2d611a46c95ceeca4d08e5ab94
Thomas Schäfer <thomasschafer@evcc.comm>
2024-03-26 09:20:05 +0100

    added neo-tree - I only use it sometimes to make it easier on others to watch ;-)

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -7,4 +7,14 @@ return {
     'MunifTanjim/nui.nvim',
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
+  keys = {
+    {
+      '<leader>fe',
+      function()
+        require('neo-tree.command').execute { toggle = true }
+      end,
+      desc = 'Explorer NeoTree (root dir)',
+    },
+    { '<leader>e', '<leader>fe', desc = 'Explorer NeoTree (root dir)', remap = true },
+  },
 }
