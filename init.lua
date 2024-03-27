49bdb65dd4136f2cfe75515c45bf784faeb4cce8
Thomas Schäfer <thomasschafer@evcc.comm>
2024-03-27 09:54:40 +0100

    added debugging

--- init.lua
+++ init.lua
@@ -545,7 +545,7 @@ require('lazy').setup({
             gopls = {
               completeUnimported = true,
               usePlaceholders = true,
-              analyse = {
+              analyses = {
                 unusedparams = true,
               },
             },
@@ -841,7 +841,7 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
 
